0ea6846c1ee326964f6ce903580859bb5190ba3d
Marcos Lacerda <marcos.lacerda@adaxo.com.br>
2017-03-24 14:48:32 -0300

    Revert test modification

--- OpenShop/BFInitViewController.m
+++ OpenShop/BFInitViewController.m
@@ -11,8 +11,6 @@
 #import "BFTabBarController.h"
 #import "BFAppDelegate.h"
 
-#import "AddNewCardViewController.h"
-
 @interface BFInitViewController ()
 
 @end
@@ -49,12 +47,7 @@ static NSString *const onboardingSegueIdentifier = @"onboardingSegue";
 - (void)viewDidAppear:(BOOL)animated {
     if(![User isLoggedIn]) {
         // login view controller
-//        [self performSegueWithIdentifier:onboardingSegueIdentifier sender:self afterDelay:launchScreenDelay];
-        
-        AddNewCardViewController *n = [[AddNewCardViewController alloc] init];
-        UINavigationController *r = [[UINavigationController alloc] initWithRootViewController:n];
-        
-        [self.navigationController presentViewController:r animated:YES completion:nil];
+        [self performSegueWithIdentifier:onboardingSegueIdentifier sender:self afterDelay:launchScreenDelay];
     }
     else {
         // tab bar controller
